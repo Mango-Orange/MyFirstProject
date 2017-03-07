82b2d7a76b08a9082b239a0825393a05acdf900b
1615396 <alex_hsmith04@hotmail.com>
2017-03-07 15:31:14 +0000

    Week 5 Test

--- Week5/index.php
+++ Week5/index.php
@@ -7,8 +7,20 @@
 <?php
 echo "Hello World";
 echo "Hello,"." "."World";
-?>
-    
+    $myname="Alex";
+    $myage=20;
+    echo "My name is". $myname. "and I am". $myage;
+    echo "I get printed";
+
+    $name="Edgar";
+
+    if($name =="Simon"){
+        print "I know you!";
+    }
+    else{
+        print "Who are you?";
+    }
+    ?>
 
 </p>
 </body>
