cc22e04c418d94cfca82a74feb8f67cabdf3c758
1615396 <alex_hsmith04@hotmail.com>
2017-03-07 16:26:02 +0000

    Week 5 Test

--- Week5/PHPINTRO/deen.php
+++ Week5/PHPINTRO/deen.php
@@ -6,18 +6,13 @@ $myage = "22";
     if($myage>"16"){
         print "You can buy Specs";
     }
-else {
-    print "You are too young still";
-}
 
-if($myage>"18"){
+elseif($myage>"18"){
     print "You can buy Specs & Mugs";
 }
-else {
-    print "You are too young still";
-}
 
-if($myage>"21"){
+
+elseif($myage>"21"){
     print "You can buy Specs, Mugs & Sausage Rolls";
 }
 else {
