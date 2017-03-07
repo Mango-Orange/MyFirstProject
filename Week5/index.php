5035f6eb7aa9de590f8038dda68b89426148f337
1615396 <alex_hsmith04@hotmail.com>
2017-03-07 15:38:20 +0000

    Week 5 Test

--- Week5/index.php
+++ Week5/index.php
@@ -9,7 +9,7 @@ echo "Hello World";
 echo "Hello,"." "."World";
     $myname="Alex";
     $myage=20;
-    print "<p> My name is ". $myname. "and I am ". $myage."</p>";
+    print "<p> My name is ". $myname. " and I am ". $myage."</p>";
     echo "I get printed";
 
     $name="Edgar";
