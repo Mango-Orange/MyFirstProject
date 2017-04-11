0217f82481761ef2821fa10a4f86748867d26366
1615396 <alex_hsmith04@hotmail.com>
2017-04-11 15:58:33 +0100

    Coursework

--- COURSEWORK/CourseworkPHP.php
+++ COURSEWORK/CourseworkPHP.php
@@ -6,7 +6,7 @@ $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
+    if($check !== false) { 
         echo "File is an image - " . $check["mine"] . ".";
         $upload0K = 1;
     } else {
