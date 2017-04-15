db4707e2cf42f8d48120ddddbef0b50d23b94638
1615396 <alex_hsmith04@hotmail.com>
2017-04-15 17:38:48 +0100

    Coursework

--- COURSEWORK/CourseworkPHP.php
+++ COURSEWORK/CourseworkPHP.php
@@ -3,25 +3,25 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$target_dir = "/CourseworkPHP".$path. "/";
-$target_file = $target_dir . "/".$path. ".jpeg";
+$target_dir = "/CourseworkPHP";
+$target_file = $target_dir . basename($_FILES["filesToUpload"]["name"]);
 $upload0K = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 // Check if image file is a actual image or fake image
 if(isset($_POST["Submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) { 
+    if($check !== false) {
         echo "File is an image - " . $check["mine"] . ".";
         $upload0K = 1;
-    } else { 
+    } else {
         echo "file is not an image.";
         $upload0K = 0;
     }
 }
 ?>
 /**
- * Created by PhpStorm.
- * User: 1615396
- * Date: 11/04/2017 
- * Time: 14:59
- */ 
+* Created by PhpStorm.
+* User: 1615396
+* Date: 11/04/2017
+* Time: 14:59
+*/ 
