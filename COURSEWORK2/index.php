f7e3beea3824ba42e241ed2a079ecfbb9a940a0a
1615396 <alex_hsmith04@hotmail.com>
2017-04-18 16:23:28 +0100

    show php errors

--- COURSEWORK2/index.php
+++ COURSEWORK2/index.php
@@ -6,46 +6,82 @@ error_reporting(E_ALL);
 
 define('INCLUDE_DIR', dirname(__FILE__) . '/inc/');
 
+
+
 $rules = array(
+
     //
+
     //main pages
+
     //
+
     'about' => "/about",
+
     'contactus' => "/contactus",
+
     'blog' => "/blog",
+
     'blog_article' => "/blog/(?'blogID'[\w\-]+)",
 
-    // 
+
+
+    //
+
     //Admin Pages
+
     //
+
     'login' => "/login",
+
     'create_article' => "/createarticle",
+
     'logout' => "/logout",
+
     //
+
     // Home Page
+
     //
+
     'home' => "/"
+
+    //
+
+    // Style
+
+    //
+
 );
 
+
+
 $uri = rtrim(dirname($_SERVER["SCRIPT_NAME"]), '/');
+
 $uri = '/' . trim(str_replace($uri, '', $_SERVER['REQUEST_URI']), '/');
+
 $uri = urldecode($uri);
 
+
+
 foreach ($rules as $action => $rule) {
+
     if (preg_match('~^' . $rule . '$~i', $uri, $params)) {
+
         include(INCLUDE_DIR . $action . '.php');
+
         exit();
+
     }
+
 }
 
+
+
 // nothing is found so handle the 404 error
+
 include(INCLUDE_DIR . '404.php');
 
-?>
 
-/**
- * Created by PhpStorm.
- * User: 1615396
- * Date: 18/04/2017
- * Time: 12:37
- */
+
+?>
