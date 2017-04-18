a26f1224679083952ded81e11a08ff647506244a
1615396 <alex_hsmith04@hotmail.com>
2017-04-18 16:27:40 +0100

    show php errors

--- COURSEWORK2/inc/home.php
+++ COURSEWORK2/inc/home.php
@@ -1,18 +1,30 @@
 <?php
 
-include("scripts/header.php");
+
+
+include ("scripts/header.php");
+
+
 
 echo "
+
 <main>
-<p>Welcome to SPUC!</p>
+
+<p>Welcome to the blog of Charles Babbage! In this blog you will see all of my insights and wonderful things</p>
+
 </main>
+
 ";
 
-include("scripts/footer.php");
+
+
+include ("scripts/footer.php");
+
+
 
 ?>
  
-/** 
+/**
  * Created by PhpStorm.
  * User: 1615396
  * Date: 18/04/2017
