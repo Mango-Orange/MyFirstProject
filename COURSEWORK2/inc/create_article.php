00726cdd6224b0cf49183ebcda87ff8dd82bee05
1615396 <alex_hsmith04@hotmail.com>
2017-04-19 17:25:18 +0100

    Upload

--- COURSEWORK2/inc/create_article.php
+++ COURSEWORK2/inc/create_article.php
@@ -1,21 +1,77 @@
 <?php
 
-include ("scripts/header.php");
+session_start();
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
+if (isset($_SESSION['username'])) //SESSION DOES EXIST
 
-<form action="upload.php" method="post" enctype="multipart/form-data">
-    Select image to upload:
-    <input type="file" name="fileToUpload" id="fileToUpload">
-    <input type="submit" value="Upload Image" name="Submit">
-</form>
+{
 
-</body>
-</html>
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+
+
+
+        include("scripts/header.php");
+
+        ?>
+
+        <main>
+
+            <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
+
+            <script>tinymce.init({selector: 'textarea'});</script>
+
+            <form action="createarticle" method="post">
+
+                <input type="text" name="articleName" placeholder="Article Name">
+
+                <textarea name="articleText"></textarea>
+
+                <input type="submit">
+
+            </form>
+
+        </main>
+
+        <?
+
+        include("scripts/footer.php");
+
+    } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+        include ('scripts/dbconnect.php');
+
+        $articleID = str_replace(' ', '-', $_POST["articleName"]);
+
+        $articleName = $_POST["articleName"];
+
+        $articleText = $_POST["articleText"];
+
+        $articleAuthor = $_SESSION['username'];
+
+
+
+        $sql = "INSERT INTO blogArticles (articleID, articleName, articleText, articleAuthor) VALUES ('". $articleID ."', '" .$articleName."', '".$articleText."', '".$articleAuthor."')";
+
+
+
+        if (mysqli_query($db, $sql)) {
+
+        } else {
+
+            echo "Error: " . $sql . "<br>Error Message:" . mysqli_error($db);
+
+        }
+
+        header("blog");
+
+    }
+
+//test
+
+} else {
+
+    header("location:login");
+
+}
+
 ?>
