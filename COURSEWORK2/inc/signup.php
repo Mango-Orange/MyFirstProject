90fae7f3373a80bf03212e20dcfbd9539c8d71a4
1615396 <alex_hsmith04@hotmail.com>
2017-04-20 13:05:30 +0100

    Upload

--- COURSEWORK2/inc/signup.php
+++ COURSEWORK2/inc/signup.php
@@ -11,119 +11,13 @@ echo "
 
                 <legend>Profile Details</legend>
 
-                <label for=\"firstName\">First name: </label>
+                <label for=\"Username\">First name: </label>
 
                 <input type=\"text\" name=\"firstName\" /><br />
-
-                <label for=\"surName\">Surname: </label>
-
-                <input type=\"text\" name=\"surName\" /><br />
-
-                <label for=\"dob\">DoB: </label>
-
-                <input type=\"date\" name=\"dob\" /><br />
 
                 <label for=\"email\">RGU email: </label>
 
                 <input type=\"email\" name=\"email\" /><br />
-
-                <label for=\"height\">Height(in centimeters): </label>
-
-                <input type=\"number\" name=\"height\" min=\"30\" max=\"350\" /><br />
-
-                <label for=\"gender\">Gender: </label>
-
-                <select name=\"gender\">
-
-                    <option value=\"male\">Male</option>
-
-                    <option value=\"female\">Female</option>
-
-                </select><br />
-
-                <label for=\"ethnicity\">Ethnicity: </label>
-
-                <select name=\"ethnicity\">
-
-                    <option value=\"white\">White</option>
-
-                    <option value=\"black\">Black</option>
-
-                    <option value=\"asian\">Asian</option>
-
-                    <option value=\"Indian\">Indian</option>
-
-                    <option value=\"notTell\">Rather not tell</option>
-
-                    <option value=\"other\">Other</option>
-
-                </select><br />
-
-                <label for=\"department\">Department: </label>
-
-                <input type=\"text\" name=\"department\" /><br />
-
-                <label for=\"programme\">Programme: </label>
-
-                <input type=\"text\" name=\"programme\" /><br />
-
-                <label for=\"maritalStatus\">Marital Status: </label>
-
-                <select name=\"maritalStatus\">
-
-                    <option value=\"married\">Married</option>
-
-                    <option value=\"single\">Single</option>
-
-                    <option value=\"divorced\">Divorced</option>
-
-                    <option value=\"widowed\">Widowed</option>
-
-                    <option value=\"civil partners\">Civil partners</option>
-
-                </select><br />
-
-            </fieldset>
-
-        </form>
-
-        <form action=\"\" method=\"post\">
-
-            <fieldset>
-
-                <legend>About me</legend>
-
-                <label for=\"interests\">Interests and hobbies: </label>
-
-                <input type=\"textarea\" name=\"interests\"><br />
-
-
-            </fieldset>
-
-        </form>
-
-
-
-        <form action=\"\" method=\"post\">
-
-            <fieldset>
-
-                <legend>Pictures</legend>
-
-                <label for=\"profilepic\">Profile picture</label><br />
-
-                <input type=\"file\" name=\"profilepic\" />
-
-                <br /><br />
-
-                <legend>Other pictures</legend>
-
-                <label for=\"otherpics\"></label>
-
-                <input type=\"file\" name=\"otherpics\" />
-
-                <input type=\"button\" name=\"upload\" value=\"Upload photo\">
-
 
             </fieldset><br />
 
