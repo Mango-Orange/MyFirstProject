d7126531153c9fa05448c7b51c412f21ba392cc5
1615396 <alex_hsmith04@hotmail.com>
2017-04-20 16:23:48 +0100

    Upload

--- COURSEWORK2/inc/signup.php
+++ COURSEWORK2/inc/signup.php
@@ -27,6 +27,31 @@ echo "
 
         </form>
 
+$sql=\"INSERT INTO users (username, password) VALUES ($name, $email)\";
+$result = $db->query($sql);
+
+    function checklogin($username, $password, $db)
+    {
+        $sql = \"SELECT * FROM users WHERE username='\" . $username . \"' and password='\" . $password . \"'\";
+        $result = $db->query($sql);
+        while ($row = $result->fetch_array()) {
+            return true;
+        }
+        return false;
+    }
+
+    if (checklogin($username, $password, $db)) {
+        session_start();
+        $_SESSION['username'] = $username;
+        header(\"location:./\");
+    } else {
+        header(\"location:login\");
+    }
+
+
+} else {
+    // this is impossible
+    print('whoops');
 
 </main>
 ";
@@ -35,7 +60,6 @@ include ("scripts/footer.php");
 
 ?>
 
-$sql="INSERT INTO users (username, password) VALUES ($name, $email)";
-$result = $db->query($sql);
+
 
 
