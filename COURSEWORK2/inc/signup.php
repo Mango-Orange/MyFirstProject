439ebdf163f71b42abdf0c50188827f174c8f210
1615396 <alex_hsmith04@hotmail.com>
2017-04-20 11:43:04 +0100

    Upload

--- COURSEWORK2/inc/signup.php
+++ COURSEWORK2/inc/signup.php
@@ -4,9 +4,138 @@ include ("scripts/header.php");
 
 echo "
 <main>
-<h2>About Me</h2>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eros quam, pharetra ornare venenatis nec, eleifend vulputate dui. Phasellus a faucibus elit. Vivamus sit amet porttitor quam. Nullam ut felis euismod, pulvinar felis a, mollis sem. Praesent non magna hendrerit, hendrerit justo consequat, convallis mauris. Donec accumsan eros nibh, non imperdiet libero tincidunt efficitur. Integer mattis augue sed pulvinar fringilla. In mollis ultrices enim, nec porttitor risus viverra luctus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla pretium sagittis ultrices. Nunc vel erat eu ante varius rhoncus id ut libero. Vestibulum ut nunc rutrum, tempus arcu a, maximus dui.</p>
-<p>Fusce ac faucibus odio, id malesuada nibh. Proin id blandit lectus. Suspendisse potenti. Integer accumsan aliquet erat at faucibus. Aliquam sed tristique nisl. Morbi at mi eu lacus rutrum facilisis nec eu diam. Mauris condimentum facilisis sodales. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+
+        <form action=\"\">
+
+            <fieldset>
+
+                <legend>Profile Details</legend>
+
+                <label for=\"firstName\">First name: </label>
+
+                <input type=\"text\" name=\"firstName\" /><br />
+
+                <label for=\"surName\">Surname: </label>
+
+                <input type=\"text\" name=\"surName\" /><br />
+
+                <label for=\"dob\">DoB: </label>
+
+                <input type=\"date\" name=\"dob\" /><br />
+
+                <label for=\"email\">RGU email: </label>
+
+                <input type=\"email\" name=\"email\" /><br />
+
+                <label for=\"height\">Height(in centimeters): </label>
+
+                <input type=\"number\" name=\"height\" min=\"30\" max=\"350\" /><br />
+
+                <label for=\"gender\">Gender: </label>
+
+                <select name=\"gender\">
+
+                    <option value=\"male\">Male</option>
+
+                    <option value=\"female\">Female</option>
+
+                </select><br />
+
+                <label for=\"ethnicity\">Ethnicity: </label>
+
+                <select name=\"ethnicity\">
+
+                    <option value=\"white\">White</option>
+
+                    <option value=\"black\">Black</option>
+
+                    <option value=\"asian\">Asian</option>
+
+                    <option value=\"Indian\">Indian</option>
+
+                    <option value=\"notTell\">Rather not tell</option>
+
+                    <option value=\"other\">Other</option>
+
+                </select><br />
+
+                <label for=\"department\">Department: </label>
+
+                <input type=\"text\" name=\"department\" /><br />
+
+                <label for=\"programme\">Programme: </label>
+
+                <input type=\"text\" name=\"programme\" /><br />
+
+                <label for=\"maritalStatus\">Marital Status: </label>
+
+                <select name=\"maritalStatus\">
+
+                    <option value=\"married\">Married</option>
+
+                    <option value=\"single\">Single</option>
+
+                    <option value=\"divorced\">Divorced</option>
+
+                    <option value=\"widowed\">Widowed</option>
+
+                    <option value=\"civil partners\">Civil partners</option>
+
+                </select><br />
+
+            </fieldset>
+
+        </form>
+
+        <form action=\"\" method=\"post\">
+
+            <fieldset>
+
+                <legend>About me</legend>
+
+                <label for=\"interests\">Interests and hobbies: </label>
+
+                <input type=\"textarea\" name=\"interests\"><br />
+
+
+            </fieldset>
+
+        </form>
+
+
+
+        <form action=\"\" method=\"post\">
+
+            <fieldset>
+
+                <legend>Pictures</legend>
+
+                <label for=\"profilepic\">Profile picture</label><br />
+
+                <input type=\"file\" name=\"profilepic\" />
+
+                <br /><br />
+
+                <legend>Other pictures</legend>
+
+                <label for=\"otherpics\"></label>
+
+                <input type=\"file\" name=\"otherpics\" />
+
+                <input type=\"button\" name=\"upload\" value=\"Upload photo\">
+
+
+            </fieldset><br />
+
+            <input type=\"submit\" name=\"submit\" value=\"Submit\" />
+
+            <br />
+
+            <a href=\"Photoupload.html\"><input type=\"button\" name=\"nextpage\" value=\"Proceed to next page\" /></a>
+
+        </form>
+
+
 </main>
 ";
 
