3b99b1df6eac6d456f8ba5f0160e88b404eabb85
1615396 <alex_hsmith04@hotmail.com>
2017-04-20 17:10:32 +0100

    Upload

--- COURSEWORK2/inc/signup.php
+++ COURSEWORK2/inc/signup.php
@@ -31,11 +31,43 @@ echo "
 </main>
 ";
 
-$sql="INSERT INTO users (username, password) VALUES ($name, $email)");
-
+?>
 include ("scripts/footer.php");
 
+
+} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+
+include("scripts/ConnectToAzureDB.php");
+
+$username = $_POST["username"];
+$password = $_POST["password"];
+
+
+function checklogin($username, $password, $db)
+{
+$sql="INSERT INTO users (username, password) VALUES ($username, $password)");
+$result = $db->query($sql);
+while ($row = $result->fetch_array()) {
+return true;
+}
+return false;
+}
+
+if (checklogin($username, $password, $db)) {
+session_start();
+$_SESSION['username'] = $username;
+header("location:./");
+} else {
+header("location:login");
+}
+
+
+} else {
+// this is impossible
+print('whoops');
+}
 ?>
 
-$sql="INSERT INTO users (username, password) VALUES ($name, $email)");
+// $sql="INSERT INTO users (username, password) VALUES ($username, $password)");
 
