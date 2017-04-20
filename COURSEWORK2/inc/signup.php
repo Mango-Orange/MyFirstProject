92fb92862c4fa337f1185d3b3e76f37c48d0858c
1615396 <alex_hsmith04@hotmail.com>
2017-04-20 21:45:33 +0100

    Upload

--- COURSEWORK2/inc/signup.php
+++ COURSEWORK2/inc/signup.php
@@ -1,5 +1,4 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 
     include("scripts/header.php");
 
@@ -15,43 +14,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         </form>
     </main>
     <?
-    
+
     include("scripts/footer.php");
-
-    } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-
-        include("scripts/ConnectToAzureDB.php");
-
-        $username = $_POST["username"];
-        $password = $_POST["password"];
-
-
-        function checklogin($username, $password, $db)
-        {
-            $sql = $this->conn->prepare("INSERT INTO users(username,password) 
-
-			                                             VALUES(:username, :password)");
-            
-            $result = $db->query($sql);
-            while ($row = $result->fetch_array()) {
-                return true;
-            }
-            return false;
-        }
-
-        if (checklogin($username, $password, $db)) {
-            session_start();
-            $_SESSION['username'] = $username;
-            header("location:./");
-        } else {
-            header("location:login");
-        }
-
-
-    } else {
-        // this is impossible
-        print('whoops');
-    }
-?>
-
