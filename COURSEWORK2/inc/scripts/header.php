7eceb86a70134d18251705dbe0691ec75150aaa8
1615396 <alex_hsmith04@hotmail.com>
2017-04-20 12:55:01 +0100

    Upload

--- COURSEWORK2/inc/scripts/header.php
+++ COURSEWORK2/inc/scripts/header.php
@@ -24,6 +24,7 @@ session_start();
             <?
             if (isset($_SESSION['username'])) {
                 echo "<li><a href='Uploadphotos'>Upload Photos</a></li>";
+                echo "<li><a href='createprofile'>Create/Edit Profile</a></li>";
                 echo "<li><a href='logout'>Logout</a></li>";
             } else {
                 echo "<li><a href='login'>Login</a></li>";
