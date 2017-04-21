e237df2da9d408a11886346252e9bf8f2abaee45
1615396 <alex_hsmith04@hotmail.com>
2017-04-21 12:22:31 +0100

    Upload

--- COURSEWORK2/inc/signup.php
+++ COURSEWORK2/inc/signup.php
@@ -30,13 +30,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         $username = $_POST["username"];
         $password = $_POST["password"];
 
-    $servername = "localhost";
     $username = "username";
     $password = "password";
     $dbname = "localdb";
 
 // Create connection
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    $conn = mysqli_connect($username, $password, $dbname);
 // Check connection
     if (!$conn) {
             die("Connection failed: " . mysqli_connect_error());
