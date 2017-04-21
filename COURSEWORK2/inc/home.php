d9032b63dfe4ea52fc70503fba58804d5e18117f
1615396 <alex_hsmith04@hotmail.com>
2017-04-21 14:28:55 +0100

    Upload

--- COURSEWORK2/inc/home.php
+++ COURSEWORK2/inc/home.php
@@ -4,7 +4,10 @@ include ("scripts/header.php");
 
 echo "
 <main>
-<p>Welcome to the blog of Charles Babbage! In this blog you will see all of my insights and wonderful things</p>
+<p>This Web application is a Photo Upload system to the web for RGU students to comment on each other’s photographs.</p>
+<p>A login system was required were RGU users can register with their e-mail addresses, a system were each user had a profile with provisions of their personal details also were they register.</p> 
+<p>The main section/purpose of this web application (SPUC) was to involve users being able to upload his or her photos to the web and allow others to comment on them.</p> 
+<p>Also users were able to store their details onto the web application and all allow other users to search for them.</p>
 </main>
 ";
 
