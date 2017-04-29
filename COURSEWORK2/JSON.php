521fc046e021a45cfc1679048b41b2a01a26fbb1
1615396 <alex_hsmith04@hotmail.com>
2017-04-29 16:59:43 +0100

    Upload

--- COURSEWORK2/JSON.php
+++ COURSEWORK2/JSON.php
@@ -1,10 +1,27 @@
 <?php
-//open connection to my mysql db
-$db = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,
-    $connectstr_dbname) or die("Error 1 " . mysqli_error($db));
+$connectstr_dbhost = '';
+$connectstr_dbname = '';
+$connectstr_dbusername = '';
+$connectstr_dbpassword = '';
+
+
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+        continue;
+    }
+
+    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+}
+
+$db = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword, $connectstr_dbname);
+
+
 
 //fetch table rows from mysql db
-$sql = "SELECT * FROM";
+$sql = "select * from localdb";
 $result = mysqli_query($db, $sql) or die("Error in selecting " . mysqli_error($db));
 
 //create an array
