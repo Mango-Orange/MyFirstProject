885a6fd31e9235271773e2609894e8e8f7351189
1615396 <alex_hsmith04@hotmail.com>
2017-04-29 16:47:01 +0100

    Upload

---
+++ COURSEWORK2/JSON.php
@@ -0,0 +1,23 @@
+<?php
+//open connection to my mysql db
+$db = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,
+    $connectstr_dbname) or die("Error 1 " . mysqli_error($db));
+
+//fetch table rows from mysql db
+$sql = "SELECT * FROM `users`";
+$result = mysqli_query($db, $sql) or die("Error in selecting " . mysqli_error($db));
+
+//create an array
+$emparray = array();
+while($row =mysqli_fetch_assoc($result))
+{
+    $emparray[] =$row;
+}
+
+echo json_encode($emparray);
+
+//close the db connection
+mysqli_close($db);
+?>
+
+
