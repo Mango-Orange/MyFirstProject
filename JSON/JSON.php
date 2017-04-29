ae37ddc21372018f0fad8f729232596cd9ad854f
1615396 <alex_hsmith04@hotmail.com>
2017-04-29 20:15:44 +0100

    Upload

--- JSON/JSON.php
+++ JSON/JSON.php
@@ -19,6 +19,21 @@ foreach ($_SERVER as $key => $value) {
 
 $db = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword, $connectstr_dbname);
 
+//fetch table rows from mysql db
+$sql = "SELECT * FROM users";
+$result = mysqli_query($connection, $sql) or die("Error in Selecting " . mysqli_error($connection));
+while ($row = $result->fetch_array());
 
+//create an array
+$emparray = array();
+while($row =mysqli_fetch_assoc($result))
+{
+    $emparray[] =$row;
+}
+
+echo json_encode($emparray);
+
+//close the db connection
+mysqli_close($db);
 ?>
 
