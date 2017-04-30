8b9e7ef10d89f154d2eca4bb0779660bb0e53006
1615396 <alex_hsmith04@hotmail.com>
2017-04-30 13:51:10 +0100

    Upload

--- JSON/JSON3.php
+++ JSON/JSON3.php
@@ -1,7 +1,35 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: 1615396
- * Date: 30/04/2017
- * Time: 13:46
- */
+
+} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+
+    include("Connectdb.php");
+
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+
+    $dbname = "localdb";
+
+
+    
+    //fetch table rows from mysql db
+    $sql = "select * from users";
+    $result = mysqli_query($dbname, $sql) or die("Error in Selecting " . mysqli_error($dbname));
+
+    //create an array
+    $emparray = array();
+    while($row =mysqli_fetch_assoc($result))
+    {
+                $emparray[] = $row;
+    }
+    echo json_encode($emparray);
+
+    //close the db connection
+    mysqli_close($dbname);
+
+else {
+    // this is impossible
+    print('whoops');
+}
+?>
