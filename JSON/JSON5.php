5c48fbb8cd5ba2119b015b659652b8892a36f534
1615396 <alex_hsmith04@hotmail.com>
2017-04-30 15:29:57 +0100

    Upload

--- JSON/JSON5.php
+++ JSON/JSON5.php
@@ -1,49 +1,20 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    include("Connectdb.php");
 
+//fetch table rows from mysql db
+$sql = "SELECT * FROM users WHERE username='" . $username . "' and password='" . $password . "'";
+$result = $db->query($sql) or die ("Error in selecting " . mysqli_error($db));
+while ($row = $result->fetch_array());
 
-    // execute if requested using HTTP GET Method
-    ?>
-    <main>
-        <form action="login" method="post">
-            <input type="text" name="username" placeholder="username"></br>
-            <input type="password" name="password" placeholder="password"></br>
-            <p><input type="submit" value="Submit"></p>
-        </form>
-    </main>
-    <?
-
-
-} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-
-    include("JSON/Connectdb.php");
-
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-
-
-    function checklogin($username, $password, $db)
-    {
-        $sql = "SELECT * FROM users WHERE username='" . $username . "' and password='" . $password . "'";
-        $result = $db->query($sql);
-        while ($row = $result->fetch_array()) {
-            return true;
-        }
-        return false;
-    }
-
-    if (checklogin($username, $password, $db)) {
-        session_start();
-        $_SESSION['username'] = $username;
-        header("location:./");
-    } else {
-        header("location:JSON5");
-    }
-
-
-} else {
-    // this is impossible
-    print('whoops');
+//create an array
+$emparray = array();
+while ($row = $result->fetch_array())
+{
+    $emparray[] =$row;
 }
+
+echo json_encode($emparray);
+
+//close the db connection
+mysqli_close($db);
 ?>
