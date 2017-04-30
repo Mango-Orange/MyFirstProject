033d20fa6d7521517f6dcb0ef980add8ae9138c1
1615396 <alex_hsmith04@hotmail.com>
2017-04-30 14:15:44 +0100

    Upload

--- JSON/JSON4.php
+++ JSON/JSON4.php
@@ -1,4 +1,4 @@
-?
+<?
 
 $connectstr_dbhost = '';
 $connectstr_dbname = '';
@@ -7,14 +7,14 @@ $connectstr_dbpassword = '';
 
 
 foreach ($_SERVER as $key => $value) {
-if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
-continue;
-}
+    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+        continue;
+    }
 
-$connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-$connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-$connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-$connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
 }
 
 $db = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword, $connectstr_dbname);
@@ -28,7 +28,7 @@ while ($row = $result->fetch_array());
 $emparray = array();
 while ($row = $result->fetch_array())
 {
-$emparray[] =$row;
+    $emparray[] =$row;
 }
 
 echo json_encode($emparray);
