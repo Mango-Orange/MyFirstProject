a9751fa799c0059247b61453fc94b0aeda5c5ef3
1615396 <alex_hsmith04@hotmail.com>
2017-04-30 14:40:38 +0100

    Upload

--- JSON/JSON5.php
+++ JSON/JSON5.php
@@ -38,7 +38,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         $_SESSION['username'] = $username;
         header("location:./");
     } else {
-        header("location:login");
+        header("location:JSON5");
     }
 
 
