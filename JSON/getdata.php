4b7f51a939f0499611796ac7a5f909a907c69dce
1615396 <alex_hsmith04@hotmail.com>
2017-05-02 12:46:01 +0100

    Upload

--- JSON/getdata.php
+++ JSON/getdata.php
@@ -2,6 +2,21 @@
 $json=file_get_contents("http://alexapp04.azurewebsites.net/JSON/JSON.php");
 $data =  json_decode($json);
 
-print_r($data);
+if (count($data->stand)) {
+    // Open the table
+    echo "<table>";
 
+    // Cycle through the array
+    foreach ($data->stand as $idx => $stand) {
+
+        // Output a row
+        echo "<tr>";
+        echo "<td>$stand->afko</td>";
+        echo "<td>$stand->positie</td>";
+        echo "</tr>";
+    }
+
+    // Close the table
+    echo "</table>";
+}
 ?>
